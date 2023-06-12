0e6b5ffaeb6b5bd2d0f6f135fc2245be1c42b5fe
melanie200422 <116139837+melanie200422@users.noreply.github.com>
2023-06-12 14:55:34 +0700

    ngochai

---
+++ app/Http/Controllers/FE/HomeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\FE;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+
+
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+//
+    /*    $role1 = \Sentinel::getRoleRepository()->createModel()->create([
+            'name' => 'Admin',
+            'slug' => 'admin',
+        ]);
+
+        $role2 = \Sentinel::getRoleRepository()->createModel()->create([
+            'name' => 'User',
+            'slug' => 'user',
+        ]);
+
+        $credentials = [
+            'email'    => 'admin@gmail.com',
+            'password' => '123',
+        ];
+
+        $user1 = \Sentinel::create($credentials);
+        $activation = \Activation::create($user1);
+        $role1->users()->attach($user1);
+
+        $credentials = [
+            'email'    => 'user@gmail.com',
+            'password' => '123',
+        ];
+        $user2 = \Sentinel::create($credentials);
+        $activation = \Activation::create($user2);
+        $role2->users()->attach($user2);
+
+*/
+
+
+return view('fe.layout.layout');
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,25 @@
 <?php
 
-use App\Http\Controllers\FE\HomeController;
+
+
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\FE\HomeController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/product/{slug}', [HomeController::class, 'productDetails'])
+        ->name('productDetails');
+
+
+
+
+
+<<<<<<< HEAD
+=======
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); DFDHJHDEG
 });
-
-Route::get('/', function () {
-    return view('admin.home');
-});
-
-
+>>>>>>> a71bf3e8e4a57c688b86289a7542ffb38894b41b
